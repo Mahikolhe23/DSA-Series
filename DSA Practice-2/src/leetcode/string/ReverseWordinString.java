09a434c97727ff75dae5ecbce0b9d37366b295f0
Mahendra Rajendra Kolhe <mkolhe23@gmail.com>
2023-01-15 11:44:42 +0530

    leetcode

---
+++ DSA Practice-2/src/leetcode/string/ReverseWordinString.java
@@ -0,0 +1,30 @@
+package leetcode.string;
+
+import java.util.Arrays;
+
+//https://leetcode.com/problems/reverse-words-in-a-string-iii/
+public class ReverseWordinString {
+
+	public static void main(String[] args) {
+		String s = "Let's take LeetCode contest";
+		System.out.println(reverseWords(s));
+	}
+
+	public static String reverseWords(String s) {
+		String[] arr = s.split(" ");
+		String[] arr1 = new String[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			String a = arr[i];
+			String b = "";
+			for (int j = a.length() - 1; j >= 0; j--) {
+				b += a.charAt(j);
+			}
+			arr1[i] = b + " ";
+		}
+		String ans = "";
+		for (int i = 0; i < arr1.length; i++) {
+			ans += arr1[i];
+		}
+		return ans;
+	}
+}

201c64b7db2330d165528311880886852e0f0b02
mahendra kolhe <mkolhe23@gmail.com>
2023-03-11 20:47:06 +0530

    leetcodez medium

---
+++ LeetCode/src/medium/StrictlyPalindromicNumber.java
@@ -0,0 +1,32 @@
+package medium;
+
+//https://leetcode.com/problems/strictly-palindromic-number/
+public class StrictlyPalindromicNumber {
+
+	public static void main(String[] args) {
+		System.out.println(isStrictlyPalindromic(9));
+	}
+
+	public static boolean isStrictlyPalindromic(int n) {
+		boolean flag = true;
+		for (int i = 2; i < n - 1; i++) {
+			if (!check(Integer.toString(n, i))) {
+				flag = false;
+				break;
+			}
+		}
+		return flag;
+	}
+
+	public static boolean check(String n) {
+		int left = 0;
+		int right = n.length() - 1;
+		while (left < right) {
+			if (n.charAt(left) != n.charAt(right))
+				return false;
+			left++;
+			right--;
+		}
+		return true;
+	}
+}

07bb62ac6f0736fc0edc01d2321dcf494fe8c59b
mahendra kolhe <mkolhe23@gmail.com>
2023-03-24 01:05:58 +0530

    gfg

--- GeekForGeeks/src/math/ReplaceZeroFromNumber.java
+++ GeekForGeeks/src/math/ReplaceZeroFromNumber.java
@@ -1,10 +1,14 @@
 package math;
 
+import java.util.Arrays;
+
 //https://practice.geeksforgeeks.org/problems/replace-all-0s-with-5/1?page=1&category[]=Arrays&sortBy=accuracy
 public class ReplaceZeroFromNumber {
 
 	public static void main(String[] args) {
-
+		int[] arr = { 23, 2, 21, 17, 16, 22, 3, 9, 10, 14, 12, 20, 11, 6, 4, 8, 7, 23, 15, 18, 13, 1, 10, 19 };
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));
 	}
 
 	static int convertfive(int num) {
